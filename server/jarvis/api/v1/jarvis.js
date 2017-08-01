fcb846ab4d2053d5d57b6543f163b929f2a4d7d8
JohnnySun <bmy001@gmail.com>
2017-08-01 18:00:05 +0800

    add actions on google jarvis webhook

---
+++ server/jarvis/api/v1/jarvis.js
@@ -0,0 +1,54 @@
+const request = require('request');
+const { ApiAiApp } = require('actions-on-google');
+const { sprintf } = require('sprintf-js');
+
+process.env.DEBUG = 'actions-on-google:*';
+
+const Actions = {
+    UNRECOGNIZED_DEEP_LINK: 'deeplink.unknown',
+    INPUT_COMMAND_LINK: 'input.command',
+    INPUT_WELCOME_LINK: 'input.welcome'
+};
+
+/**
+ * Greet the user and direct them to next turn
+ * @param {ApiAiApp} app ApiAiApp instance
+ * @return {void}
+ */
+const unhandledDeepLinks = app => {
+    /** @type {string} */
+    const rawInput = app.getRawInput();
+    const response = sprintf("Jarvis dont know what you say. %s", rawInput);
+    console.log(`Jarvis response unknown: ${response}`);
+    app.ask(response);
+};
+
+const inputCommandLinks = app => {
+    const rawInput = app.getRawInput();
+    const response = sprintf("Jarvis is very early version and cant response this question right now.");
+    console.log(`Jarvis response: ${response}`);
+    app.ask(response);
+};
+
+const inputWelcomeLinks = app => {
+    const response = "Hello Johnny. This is very early version, jarvis may be unstable. Version: 0.0.1. ";
+    console.log(`Jarvis : ${response}`);
+    app.ask(response);
+};
+
+const actionMap = new Map();
+actionMap.set(Actions.UNRECOGNIZED_DEEP_LINK, unhandledDeepLinks);
+actionMap.set(Actions.INPUT_COMMAND_LINK, inputCommandLinks);
+actionMap.set(Actions.INPUT_WELCOME_LINK, inputWelcomeLinks);
+
+
+function post(req, res, next) {
+    const app = new ApiAiApp({ req, res });
+    console.log(`Request headers: ${JSON.stringify(req.headers)}`);
+    console.log(`Request body: ${JSON.stringify(req.body)}`);
+    app.handleRequest(actionMap);
+}
+
+module.exports = {
+    post: post
+};
--- server/jarvis/routes/api.js
+++ server/jarvis/routes/api.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var get_aqi = require('../api/v1/get_aqi');
+var jarvis = require('../api/v1/jarvis')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.send('respond with a resource');
 });
 
-router.get('/get_aqi', function (req, res, next) {
+router.get('/v1/get_aqi', function (req, res, next) {
     get_aqi.get(req, res, next);
 });
+
+router.post('/v1/jarvis', function (req, res, next) {
+    jarvis.post(req, res, next);
+})
 
 module.exports = router;
